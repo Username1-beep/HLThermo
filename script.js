1a101f3662f5c8d332f2ac8fa66fc6076919e7ef
Username1-beep <multifructina@gmail.com>
2023-11-21 01:44:07 +0300

    вроде все 2.0

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const sliders = document.querySelectorAll('.slider');
-  
-//     sliders.forEach(function(slider) {
-//       const slides = slider.querySelectorAll('.slide');
-//       let currentIndex = 0;
-  
-//       function showSlide(index) {
-//         const translateValue = -index * 100 + '%';
-//         slider.style.transform = 'translateX(' + translateValue + ')';
-//       }
-  
-//       function nextSlide() {
-//         currentIndex = (currentIndex + 1) % slides.length;
-//         showSlide(currentIndex);
-//       }
-  
-//       setInterval(nextSlide, 5000);
-//     });
-//   });
 
   let currentImageIndex = 1;
   const totalImages = 9;
